303123454944d520889fd201e96f1e80f4afa1f0
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2020-10-25 23:50:37 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add { 
+    public static void main(String[] args) 
+    { 
+        int num1 = 10, num2 = 20, sum ;
+        System.out.println("num1 = " + num1); 
+        System.out.println("num2 = " + num2); 
+        sum = num1 + num2; 
+        System.out.println("The total value of  = " + sum); 
+    } 
+} 
