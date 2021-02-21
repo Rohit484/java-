b455f73c9fe70e3b5e679f16eff6294692af18ec
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-02-21 19:13:43 +0530

    Create greek7.java

---
+++ greek7.java
@@ -0,0 +1,33 @@
+class Solution 
+{ 
+    static int catchThieves(char arr[], int n, int k) 
+	{ 
+		int result = 0; 
+		ArrayList<Integer> thief = new ArrayList<Integer>(); 
+		ArrayList<Integer> police = new ArrayList<Integer>(); 
+
+		for (int i = 0; i < n; i++) { 
+			if (arr[i] == 'P') 
+			police.add(i); 
+			else if (arr[i] == 'T') 
+			thief.add(i); 
+		} 
+
+		// track lowest current indices of 
+        // thief: thi[l], police: pol[r] 
+		int l = 0, r = 0; 
+		while (l < thief.size() && r < police.size()) { 
+			// can be caught
+			if (Math.abs(thief.get(l) - police.get(r)) <= k) { 
+				result++; 
+				l++; 
+				r++; 
+			} 
+			else if (thief.get(l) < police.get(r)) 
+				l++; 
+			else
+				r++; 
+		} 
+		return result; 
+	} 
+} 
