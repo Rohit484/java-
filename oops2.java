8ae0092fdb3e6d054c4ca04eb2cf3b26f1d9f04f
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:16:59 +0530

    Create oops2.java

---
+++ oops2.java
@@ -0,0 +1,19 @@
+
+class book {
+	int bookno,price;
+	String title;
+
+}
+class oops2
+{
+	public static void main(String[]args)
+	{
+		book b1=new book();
+		b1.bookno=408;
+		b1.price=500;
+		b1.title="Lets Learn Java ";
+		System.out.println("Book No: "+b1.bookno);
+		System.out.println("Book'Price: "+b1.price);
+		System.out.println("Book'sTitle: "+b1.title);
+	}
+	}
