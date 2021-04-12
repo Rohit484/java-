15fe2f319d236ebe11128d32ef16ce3b0394f0c2
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:15:34 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,12 @@
+
+class Area
+{
+	public static void main(String args[])
+	   {
+	      int radius = 3;
+	      double area = Math.PI * (radius * radius);
+	      System.out.println(" Area: " + area);
+	      double circumference= Math.PI * 2*radius;
+	      System.out.println( "The circumference is:"+circumference) ;
+	   }
+}
