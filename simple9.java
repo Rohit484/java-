280c7059a0434b79e840cda24abfb399f31d1c95
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:15:07 +0530

    Create simple9.java

---
+++ simple9.java
@@ -0,0 +1,16 @@
+
+public class simple9 {
+	
+		public static void main(String[]args)
+		{
+			System.out.println("Hello IGEC ");
+			System.out.println("IGEC GOVET ");
+			System.out.println("Hello SDL ");
+			System.out.println("Jabalpur sdl ");
+			System.out.println("Gohparu ");
+			System.out.println("Hello World!! ");
+		}
+	
+	
+
+}
