0af518a9ae3b4d3634cd8d553b447e6cff50042e
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:16:30 +0530

    Create oops.java

---
+++ oops.java
@@ -0,0 +1,17 @@
+
+  class Student {
+      int rollno;
+      String name;
+      
+}
+ class oops
+ {
+	 public static void main(String[]args)
+	 {
+		 Student s1=new Student();
+		 s1.rollno=101;
+		 s1.name="Rohit'Aptech";
+		 System.out.println("Roll No: "+s1.rollno);
+		 System.out.println("Name is: "+s1.name);
+	 }
+ }
