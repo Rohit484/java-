8bceedbc215b07f7c32eee75d326a19afd9dc0c2
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:11:37 +0530

    Create simple.java

---
+++ simple.java
@@ -0,0 +1,16 @@
+
+public class Simple {
+
+	public static void main(String[] args) {
+		byte age=22;
+		int b;
+		b=9;
+		char a; 
+		a='R';
+		System.out.println("byte"+age);
+		System.out.println("int"+b);
+		System.out.println("char"+a);
+
+	}
+
+}
