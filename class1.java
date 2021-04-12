9c4b1fd1cc87a0e4295fc29b1f3ad6be108baeb9
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:16:05 +0530

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,48 @@
+ class class2 {
+	int rollno;
+	String name;
+
+}
+ class class1
+{
+	public static void main(String[]args)
+	{
+		class2 s1=new class2();
+		class2 s2=new class2();
+		class2 s3=new class2();
+		class2 s4=new class2();
+		class2 s5=new class2();
+		class2 s6=new class2();
+		class2 s7=new class2();
+		s1.rollno=101;
+		s1.name="Rohit'Aptech: ";
+		s2.rollno=102;
+		s2.name="Rohit'IGEC: ";
+		s3.rollno=103;
+		s3.name="Rohit'SDL: ";
+		s4.rollno=104;
+		s4.name="Arvind:";
+		s5.rollno=105;
+		s5.name="BK: ";
+		s6.rollno=106;
+		s6.name="Aman: ";
+		s7.rollno=107;
+		s7.name="Golu: ";
+		
+		System.out.println("Roll No: "+s1.rollno);
+		System.out.println("Name: "+s1.name);
+		System.out.println("Roll No:"+s2.rollno);
+		System.out.println("Name: "+s2.name);
+		System.out.println("Roll No: "+s3.rollno);
+		System.out.println("Name: "+s3.name);
+		System.out.println("Roll No: "+s4.rollno);
+		System.out.println("Name: "+s4.name);
+		System.out.println("Roll No: "+s5.rollno);
+		System.out.println("Name: "+s5.name);
+		System.out.println("Roll No: "+s6.rollno);
+		System.out.println("Name: "+s6.name);
+		System.out.println("Roll No: "+s7.rollno);
+		System.out.println("Name: "+s7.name);
+		System.out.println("Rohit"+s7.name);
+	}
+	}
