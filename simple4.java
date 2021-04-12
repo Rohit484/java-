0e380736c9b87bd8130fd2707da2f914d2f7769a
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:12:55 +0530

    Create simple4.java

---
+++ simple4.java
@@ -0,0 +1,11 @@
+
+public class simple4 {
+       public static void main(String[]args)
+       {
+    	   int i;
+    	   for(i=1;i<=10;i++)
+    	   {
+    		   System.out.println("Hello Rohit: "+i);
+    	   }
+       }
+}
