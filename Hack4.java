b1bfbfb22016ea9f3166736c7af194f78d6edcd1
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:05:36 +0530

    Create Hack4.java

---
+++ Hack4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hack {
+	public static void main(String[] args) {
+	    
+		Scanner s = new Scanner(System.in); 
+		
+		int num1 = s.nextInt(); // Get first integer
+		int num2 = s.nextInt(); // Get second integer
+		
+		int sum = 0;
+		// Write the logic to add these integers here
+		
+		
+		
+		
+		
+		// Print the sum
+		System.out.println(sum);
+		s.close();
+	}
+
+}
