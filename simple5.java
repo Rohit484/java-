b34f2eadb3e9cbe8ce5169789713d844dbdfb5be
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:13:19 +0530

    Create simple5.java

---
+++ simple5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class simple5 {
+        public static void main(String[]args)
+        {
+        	
+        	Scanner sc= new Scanner(System.in);
+        	System.out.println("Enter The Two Values");
+        	int a=sc.nextInt();
+        	int b=sc.nextInt();
+        	int c=a+b;
+        	System.out.println("Here Your Total sum is: "+c);
+        }
+}
