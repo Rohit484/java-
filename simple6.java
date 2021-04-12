b0646e6ea8a875fd20fe8135db5d896fcff66370
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:13:43 +0530

    Create simple6.java

---
+++ simple6.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class simple6 {
+	public static void main(String[]args)
+	{
+		byte age;
+		age=22;
+		
+		long pop;
+		pop=89;
+		/*
+		short salary;
+		salary=-45000;
+		int profit;
+		profit=98;*/
+		float temp;
+		temp=90;
+		double pi;
+		pi=8900;
+		boolean ans;
+		ans=false;
+		char ch;
+		ch='R';
+		String str;
+		str="10,Cvil Line,Sagar(M.P)";
+		System.out.println("Byte:"+age);
+		System.out.print("long:"+pop);
+		System.out.println("Float:"+temp);
+		System.out.println("Double:"+pi);
+		System.out.println("Boolean:"+ans);
+		System.out.println("Char:"+ch);
+		System.out.println("String:"+str);
+		/*System.out.println("Int: "+profit);
+		System.out.println("Short: "+salary);*/
+	}
+
+}
