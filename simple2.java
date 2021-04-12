0e9f9a0c3df644da76f9ca6dbdb65950b5a9b730
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-12 10:11:59 +0530

    Create simple2.java

---
+++ simple2.java
@@ -0,0 +1,10 @@
+
+public class simple2 {
+      public static void main(String[]args)
+      {
+    	  System.out.println("Hello Rohit");
+    	  int a;
+    	  a=9;
+    	  System.out.println(a);
+      }
+}
