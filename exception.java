03458994014f11c45d99f410dd3aef7a43406cab
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-18 21:11:18 +0530

    Create exception.java

---
+++ exception.java
@@ -0,0 +1,20 @@
+
+public class exception {
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=0;
+		int c;
+		try
+		{
+			c=a/b;
+			System.out.println("Here your Division is: "+c);	
+		}
+		catch(Exception e)
+		{
+			System.out.println("Here is: ");
+		}
+		
+	}
+
+}
