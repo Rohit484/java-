0e250b575b45dde73705267a96c773e93c56df80
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-20 14:53:25 +0530

    Create Max2.java

---
+++ Max2.java
@@ -0,0 +1,15 @@
+
+public class Max2 {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,50};
+		int max=a[0];
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]>max)
+				max=a[i];
+		}
+		System.out.println("Max Value is "+max);
+	}
+
+}
