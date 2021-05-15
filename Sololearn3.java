1ea7de59618810d83ddd66bd814d7621e18afb80
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-05-15 18:02:09 +0530

    Add files via upload

---
+++ Sololearn3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Sololearn3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 11; int b = 12; int c = 40;
+switch (a) {
+ case 40:
+   System.out.println(b);
+   break;
+ default:
+   System.out.println(c);
+}
+    }
+    
+}
