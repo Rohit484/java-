592c8b22e26d4ee4853b6b9f13c5d831194f4b9f
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-05-15 17:41:07 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+        
+   int val = new Scanner(System.in).nextInt();
+   int days = 24 * 60 * 60 * val ;
+   System.out.println(days);
+    }
+}
