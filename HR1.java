967d095f9eba12b5af00b9bccb54ac0a81e798c6
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-08-05 11:49:45 +0530

    Add files via upload

---
+++ HR1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ROHIT PANDEY
+ */
+import java.util.Scanner;
+public class HR1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
